2a8efff831e606073bc828f23a6143ff257d12e1
nick <ncerne00@gmail.com>
2025-03-27 20:13:00 -0400

    Fixing MDX code blocks

--- src/components/content/MDXComponents.tsx
+++ src/components/content/MDXComponents.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Title, Text, Code, List, Anchor, Image } from '@mantine/core';
+import { Title, Text, Code, List, Anchor, Image, Box } from '@mantine/core';
 
 /* This needs work */
 const MDXComponents = {
@@ -8,7 +8,30 @@ const MDXComponents = {
   h3: (props: any) => <Title order={3} my="sm" {...props} />,
   h4: (props: any) => <Title order={4} my="xs" {...props} />,
   p: (props: any) => <Text my="md" {...props} />,
-  code: (props: any) => <Code {...props} />,
+  /* Checks if the code block is a block or inline code */
+  code: (props: any) => {
+      /* Check if this is a code block (has className with language) or inline code */
+      if (props.className) {
+        /* This is a code block (```code```) */
+        const language = props.className.replace('language-', '');
+        return (
+          <Box 
+            my="md" 
+            style={{
+              backgroundColor: 'var(--mantine-color-gray-1)',
+              borderRadius: 'var(--mantine-radius-md)',
+              padding: 'var(--mantine-spacing-md)',
+              overflow: 'auto',
+            }}
+          >
+            <Code block language={language} {...props} />
+          </Box>
+        );
+      }
+      
+      /* This is inline code (`code`) */
+      return <Code {...props} />;
+    },
   ul: (props: any) => <List {...props} />,
   ol: (props: any) => <List type="ordered" {...props} />,
   li: (props: any) => <List.Item {...props} />,
