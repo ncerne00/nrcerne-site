74ae8f4bceedbee83c85b05e6781dc0ce5585d64
nick <ncerne00@gmail.com>
2025-03-28 15:13:59 -0400

    async bug fixes

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -12,8 +12,9 @@ export async function generateStaticParams() {
   }));
 }
 
-export default async function BlogPostPage({ params }: { params: { slug: string } }) {
-  const post = await getPostBySlug(params.slug);
+export default async function BlogPostPage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
+  const post = await getPostBySlug(slug);
   
   if (!post) {
     return notFound();
